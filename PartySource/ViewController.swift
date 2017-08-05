294d3abe5d012f5b06790ee4e27ec4ce091fa410
Richard Oros <richardoros@Richards-MacBook-Pro.local>
2017-08-05 22:23:44 +0200

    Initial Commit

---
+++ PartySource/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PartySource
+//
+//  Created by Richard Oros on 05/08/2017.
+//  Copyright © 2017 CRN Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
