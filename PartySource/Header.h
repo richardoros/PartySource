cd9c7d5b018ae77ada42dcf0e75bde681235114a
Richard Oros <richardoros@Richards-MacBook-Pro.local>
2017-08-07 11:15:33 +0200

    Update 0.2

---
+++ PartySource/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  Calendar
+//
+//  Created by Niraj Sigdel on 05/08/2017.
+//  Copyright © 2017 Niraj Sigdel. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
--- PartySource/ProfileView.swift
+++ PartySource/ProfileView.swift
@@ -1,54 +1,54 @@
+////
+////  HomeViewController.swift
+////  PartySource
+////
+////  Created by Richard Oros on 21/05/2017.
+////  Copyright © 2017 Richard, Niraz and Casian. All rights reserved.
+////
 //
-//  HomeViewController.swift
-//  PartySource
+//import Foundation
+//import Firebase
+//import FirebaseAuth
 //
-//  Created by Richard Oros on 21/05/2017.
-//  Copyright © 2017 Richard, Niraz and Casian. All rights reserved.
+//class ProfileView: UIView {
+//    
+//    
+//    @IBAction func logOutAction(_ sender: UIButton) {
+//        if Auth.auth().currentUser != nil {
+//            do {
+//                try Auth.auth().signOut()
+//                let vc = self.storyboard?.instantiateViewController(withIdentifier: "Profile")
+//                self.present(vc!, animated: true, completion: nil)
 //
-
-import Foundation
-import Firebase
-import FirebaseAuth
-
-class ProfileView: UIView {
-    
-    
-    @IBAction func logOutAction(_ sender: UIButton) {
-        if Auth.auth().currentUser != nil {
-            do {
-                try Auth.auth().signOut()
-                let vc = self.storyboard?.instantiateViewController(withIdentifier: "Profile")
-                self.present(vc!, animated: true, completion: nil)
-
-                
-            } catch let error as NSError {
-                print(error.localizedDescription)
-                print("Couldn't find Home or Login storyboard ID")
-            }
-        }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        //Looks for single or multiple taps.
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ProfileView.dismissKeyboard))
-        
-        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
-        //tap.cancelsTouchesInView = false
-        
-        view.addGestureRecognizer(tap)
-    }
-    
-    //Calls this function when the tap is recognized.
-    func dismissKeyboard() {
-        //Causes the view (or one of its embedded text fields) to resign the first responder status.
-        view.endEditing(true)
-    }
-    
-    
-    
-    
-    
-    
-}
+//                
+//            } catch let error as NSError {
+//                print(error.localizedDescription)
+//                print("Couldn't find Home or Login storyboard ID")
+//            }
+//        }
+//    }
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        //Looks for single or multiple taps.
+//        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ProfileView.dismissKeyboard))
+//        
+//        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+//        //tap.cancelsTouchesInView = false
+//        
+//        view.addGestureRecognizer(tap)
+//    }
+//    
+//    //Calls this function when the tap is recognized.
+//    func dismissKeyboard() {
+//        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+//        view.endEditing(true)
+//    }
+//    
+//    
+//    
+//    
+//    
+//    
+//}
